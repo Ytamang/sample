0f6ea7d64e63ef1023fa84d5f917a5728837b61d
yubraj.tamang <yubraj.tamang@kroger.com>
2019-11-25 13:27:06 -0500

    added stash to feature

--- src/main/java/com/report/reporting/controllers/UserController.java
+++ src/main/java/com/report/reporting/controllers/UserController.java
@@ -27,46 +27,7 @@ public class UserController {
     public UserController(ApiService apiService) {
 //    	this.crRepository=crRepository;
     	this.apiService=apiService;
-    	try{
-    		 String uuid = UUID.randomUUID().toString();
-    	        CameraStatus testUser = new CameraStatus(uuid,1L,new Timestamp(System.currentTimeMillis()), 44L,"1004","2004");
-    	        CameraStatus testUser2 = new CameraStatus(uuid,1L,new Timestamp(System.currentTimeMillis()), 44L,"1003","2003");
-    	        CameraStatus testUser3= new CameraStatus(uuid,1L,new Timestamp(System.currentTimeMillis()), 44L,"1005","2005");
-    	        CameraStatus testUser4= new CameraStatus(uuid,1L,new Timestamp(System.currentTimeMillis()), 44L,"1006","2006");
-    	        CameraStatus testUser5= new CameraStatus(uuid,1L,new Timestamp(System.currentTimeMillis()), 44L,"1007","2007");
-    	        CameraStatus testUser6= new CameraStatus(uuid,1L,new Timestamp(System.currentTimeMillis()), 44L,"1008","2008");
-    	        CameraStatus testUser7= new CameraStatus(uuid,2L,new Timestamp(System.currentTimeMillis()), 44L,"1009","2009");
-    	        CameraStatus testUser8= new CameraStatus(uuid,2L,new Timestamp(System.currentTimeMillis()), 44L,"1010","2010");
-    	        CameraStatus testUser9= new CameraStatus(uuid,3L,new Timestamp(System.currentTimeMillis()), 44L,"1001","2001");
-    	        CameraStatus testUser10= new CameraStatus(uuid,3L,new Timestamp(System.currentTimeMillis()), 44L,"1002","2002");
-    	        CameraStatus testUser11= new CameraStatus(uuid,4L,new Timestamp(System.currentTimeMillis()), 44L,"1011","2011");
-    	        CameraStatus testUser12 = new CameraStatus(uuid,4L,new Timestamp(System.currentTimeMillis()), 44L,"1012","2012");
-    	        // Save the User class to the Azure database.
-//    	        userRepository.save(testUser);
-    	        testUser= apiService.saveCameraStatus(testUser);
-    	/*        apiService.saveCameraStatus(testUser);
-    	        apiService.saveCameraStatus(testUser2);
-    	        apiService.saveCameraStatus(testUser3);
-    	        apiService.saveCameraStatus(testUser4);
-    	        apiService.saveCameraStatus(testUser5);
-    	        apiService.saveCameraStatus(testUser6);
-    	        apiService.saveCameraStatus(testUser7);
-    	        apiService.saveCameraStatus(testUser8);
-    	        apiService.saveCameraStatus(testUser9);
-    	        apiService.saveCameraStatus(testUser10);
-    	        apiService.saveCameraStatus(testUser11);
-    	        apiService.saveCameraStatus(testUser12);*/
-System.out.println("################################## #                 ####"+testUser.getId());
-    	        // Retrieve the database record for the User class you just saved by ID.
-CameraStatus result = apiService.findById(testUser.getId());
-// List<CameraStatusPOJO> dd = crRepository.findByStoreNumber(1L);
-    	        // Display the results of the database record retrieval.
-    	        System.out.println(" ==========\nSaved user is: " + result + "\n");
-    	}catch (Exception e) {
-    		e.printStackTrace();
-    	}
 
-        
     }
 
     @CrossOrigin
@@ -94,23 +55,7 @@ CameraStatus result = apiService.findById(testUser.getId());
     	System.out.println("---------------results  -----------> "+vos);
         return vos;
     }
-   /* @CrossOrigin
-    @GetMapping("/store/{storeId}")
-    public CameraStatusVO getDetailsByStoreId(@PathVariable("storeId") Long storeId) {
-        CameraStatus cs =crRepository.findByStoreNumber(storeId).get(0);
-        
-        
-        CameraStatusVO vo = new CameraStatusVO();
-		vo.setDivisionId(cs.getDivisionNumber());
-		vo.setSensorId(cs.getBayNumber());
-		
-		Timestamp ts=new Timestamp(System.currentTimeMillis());
-		vo.setCamstatus( ts.getTime() - cs.getStartTime().getTime() <= 60*60*24*1000? "GOOD" : "BAD");
-		vo.setFacilityId(cs.getFacilityId());
-		vo.setInsertTs(cs.getStartTime());
-		return vo;
-    }
-    */
+
     @CrossOrigin
     @PostMapping("/listStore")
     public List<CameraStatusVO> returnStoreCameraStatus(@RequestBody CameraStatusRequest request) {
@@ -133,12 +78,6 @@ CameraStatus result = apiService.findById(testUser.getId());
 
     }
 
-//    @CrossOrigin
-//    @GetMapping("/getCameraStatus")
-//    public List<CameraStatusPOJO> getCameraStatus() {
-//        return apiService.getCameraStatus();
-//    }
-    
     @CrossOrigin
     @GetMapping("/getDivisions")
     public List<String> getAllDivisions() {
@@ -147,12 +86,16 @@ CameraStatus result = apiService.findById(testUser.getId());
     }
 
     @CrossOrigin
-    @GetMapping("/getFacilities")
+    @GetMapping("/getFacilities}")
     public List<String> getAllFacilities() {
     	System.out.println("**********     getFacilities Controller      *************");
     	return apiService.getAllFacilities();
     }
-    
-    
+
+    @CrossOrigin
+	@GetMapping("/hello")
+    public String hello(){
+    	return "hello";
+	}
     
 }
